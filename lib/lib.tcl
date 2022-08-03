6d26adcb1b1ef7e68437381f787d046ab51f4211
Zachary Yedidia <zyedidia@gmail.com>
2022-08-03 14:57:36 -0700

    Add let

--- lib/lib.tcl
+++ lib/lib.tcl
@@ -1,3 +1,14 @@
 proc setdef { var val } {
     if {![uplevel "info exists $var"]} { uplevel "set $var \"$val\"" }
 }
+
+proc let {varName eq args} {
+    upvar $varName v
+    if {$eq == "="} {
+        set v [expr $args]
+    } else {
+        if {$eq == "?="} {
+            setdef v [expr $args]
+        }
+    }
+}
